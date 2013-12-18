3e290a7b8d5015b9b993abd7bc02668246825427
Leonardo <leo@radbitt.com>
2013-12-17 16:37:51 -0800

    Master

--- front-page.php
+++ front-page.php
@@ -1,43 +1,20 @@
-<?php get_header(); ?>
 
-<?php $args = array( 'post_type' => 'trips', 'posts_per_page' => 4 );
+<html>
 
-	$loop = new WP_Query( $args );  ?>
+<head>
 
-		<div id="radSlider" class="postContainer">
+</head>
 
-			<div class="active rad-slide" style="background-image: url(http://www.dirtmiles.com/wp-content/themes/motorSportClub/img/grandCanyonP7_MindBlown.jpg);">&nbsp</div>
+<body>
 
-			<?php while ( $loop->have_posts() ) : $loop->the_post(); $randomPictures = rand(1,10); ?>
+		<div id="rad-slider">
 
-			<div class="rad-slide" style="background-image: url(<?php the_field('trip_picture_' . $randomPictures);?>);">&nbsp</div>
+			<div class="active rad-slide" style="background-image: url('');">&nbsp</div>
 
-			<?php endwhile; ?>
+			<div class="rad-slide" style="background-image: url('');">&nbsp</div>
 
 		</div>
 
-<?php if(have_posts()) : while ( have_posts()) : the_post(); ?>
+</body>
 
-	<div class="postContainer">
-
-		<h3><?php the_date('M d', '<div class="theDate">', '</div>');?><a href="<?php the_permalink();?>">
-		<?php the_title(); ?></a></h3> 
-
-		<?php the_post_thumbnail('thumbnail'); ?>
-
-		<?php the_content(); ?>
-
-		<div class="postFooter">
-
-		<p>Author : <?php the_author() ?></p><a href="<?php comments_link(); ?>">Discussion</a>
-		
-		</div>
-
-	</div>
-
-<?php endwhile; else: ?>
-
-	<p>There are no posts here</p>
-
-<?php endif; ?>
-<?php get_footer(); ?>
+</html>
--- main.js
+++ main.js
@@ -1,26 +1,25 @@
 jQuery(document).ready(function() {
 
-	
 	var i = 0 
 
 	function imgCount() {
 
-    	var n = jQuery('div#radSlider div').length;
+    	var n = jQuery('div#rad-slider div').length;
 
     	return n;
     }
 
-    function slideSwitch() {
+    function imgSwitch() {
 
     	if (i >= imgCount()) {
 
 			i = 0 ; 
 
-			jQuery('div#radSlider div:first-child').addClass('active');
+			jQuery('div#rad-slider div:first-child').addClass('active');
 
 		} else {
 
-	    jQuery('div#radSlider div.active').removeClass('active').next('div').addClass('active');
+	    jQuery('div#rad-slider div.active').removeClass('active').next('div').addClass('active');
 
 	    }
 
@@ -28,30 +27,7 @@ jQuery(document).ready(function() {
 
     }
 
-	setInterval(function() { slideSwitch();} , 5000);
-
-	/* Navigation Bar Detach and Scroll */
-
-	jQuery(window).scroll(function(){
-
-    if  (jQuery(window).scrollTop() >= 201){
-
-         jQuery('.menu-navigation-container').css({'position':'fixed','top':5,'max-width':'1176px','margin-left':'11px'});
-
-    } else {
-
-         jQuery('.menu-navigation-container').css({'position':'relative', 'top':0,'max-width':'none','margin':'inherit'});
-
-        }
-	});
-
-/* loading-message */
-	jQuery(window).load(function(){
-
-    jQuery('#loader').fadeOut(500);
-
-	});
-
+	setInterval(function() { imgSwitch();} , 5000);
 
 });
 
